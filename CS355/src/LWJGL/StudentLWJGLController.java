a9a79730da17e851ff6662d789eb2f5e2ee7a277
jpetmecky23 <jpetmecky23@gmail.com>
2015-03-13 20:34:55 -0600

    EvenMore

--- CS355/src/LWJGL/StudentLWJGLController.java
+++ CS355/src/LWJGL/StudentLWJGLController.java
@@ -122,11 +122,15 @@ public class StudentLWJGLController implements CS355LWJGLController
         {
             System.out.println("You are pressing O!");
             glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            gluPerspective(60.0f, 1.33f, 5f, 100f);
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_P)) 
         {
             System.out.println("You are pressing P!");
             glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            gluPerspective(60.0f, 1.33f, 5f, 100f);
         }
 
     }
